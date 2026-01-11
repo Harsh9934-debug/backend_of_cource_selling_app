b9e603049f62d7e843f3a467e8cf84dc57fa439d
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2026-01-11 23:14:00 +0530

    made this more solid

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ const { userRouter } = require("./routes/user");
 const { coursesRouter } = require("./routes/courses");
 const { adminRouter } = require("./routes/admin");
 
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 const app = express();
 
 app.use("/user", userRouter);
 app.use("/courses", coursesRouter);
 app.use("/admin", adminRouter);
 
-async function main(){
-    await mongoose.connect(process.env.MONGO_URL)
-    .then(() => {
-        console.log("Connected to MongoDB")
-    })
-    .catch((err) => {
-        console.log(err,"App Crashed")
-    })
-    app.listen(3000, () => {
-    console.log("Server is rinning in the localhost 3000")
-})
+async function main() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to MongoDB");
+
+        app.listen(3000, () => {
+            console.log("Server is running on localhost:3000");
+        });
+    } catch (error) {
+        console.error("Failed to connect to MongoDB chack the url properly");
+        process.exit(1);
+    }
 }
 
 
