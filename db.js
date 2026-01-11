be31840a681e232325011de49c03ac57af379ba0
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2026-01-11 22:35:51 +0530

    Updated it

--- db.js
+++ db.js
@@ -1,4 +1,7 @@
 const {Schema} = require("mongoose");
+const mongoose = require("mongoose");
+const { ObjectId } = require("mongoose");
+
 const userSchema = new Schema({
     email: String,
     password: String,
@@ -18,18 +21,23 @@ const courseSchema = new Schema({
     description: String,
     price: Number,
     image: String,
-    creatorId: String,
+    creatorId: ObjectId,
 });
 
 const purchageSchema=new Schema({
-    title:String,
-    description:String,
-    price:Number,
-    image:String,
-    creatorId:String,
+    userId:ObjectId,
+    courseId:ObjectId,
 });
 
 const usermodel=mongoose.model("user",userSchema);
 const adminmodel=mongoose.model("admin",adminSchema);
 const coursemodel=mongoose.model("course",courseSchema);
 const purchagemodel=mongoose.model("purchage",purchageSchema);
+
+
+module.exports={
+    usermodel,
+    adminmodel,
+    coursemodel,
+    purchagemodel
+}
